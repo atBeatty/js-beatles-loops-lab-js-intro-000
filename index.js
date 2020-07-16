dfd8466380eea70b9e92959c8f9b68ad69a1f104
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-16 01:50:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var resArr = [];
+
+  if (musicians.length === instruments.length) {
+    for (var i = 0; i < instruments.length; i++) {
+      var string = musicians[i] + ' plays ' + instruments[i];
+      resArr.push(string);
+      console.log(resArr);
+    }
+  }
+}
+
+var musiciansInput = ['bon', 'slug', 'eric clapton']
+var instrumentsInput = ['guitar', 'microphone', 'piano']
+
+theBeatlesPlay(musiciansInput, instrumentsInput)
