80ca0d2f2452830722d06342920f8a8cf1332c19
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-16 01:51:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ function theBeatlesPlay(musicians, instruments) {
 
   if (musicians.length === instruments.length) {
     for (var i = 0; i < instruments.length; i++) {
-      var string = musicians[i] + ' plays ' + instruments[i];
+      var string = '';
+      string = musicians[i] + ' plays ' + instruments[i];
       resArr.push(string);
-      console.log(resArr);
     }
   }
 }
 
-var musiciansInput = ['bon', 'slug', 'eric clapton']
-var instrumentsInput = ['guitar', 'microphone', 'piano']
+// var musiciansInput = ['bon', 'slug', 'eric clapton']
+// var instrumentsInput = ['guitar', 'microphone', 'piano']
 
-theBeatlesPlay(musiciansInput, instrumentsInput)
